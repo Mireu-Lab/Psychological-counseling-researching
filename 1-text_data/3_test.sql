f72dd0f311f033aca662438237fe9129d8045baf
Mireu Lim <97509351+Mireu-Lab@users.noreply.github.com>
2026-03-07 07:21:32 +0000

    Add Jupyter notebook for GCP BigQuery data clustering and SQL query for morpheme classification

---
+++ 1-text_data/3_test.sql
@@ -0,0 +1,8 @@
+SELECT *
+FROM `testprojects-453622.Psychological_counseling_data.morpheme_classification`
+-- WHERE
+--     file_code="A094" AND
+--     session_no=1
+ORDER BY
+    end_content_index ASC,
+    timeline_index ASC;
