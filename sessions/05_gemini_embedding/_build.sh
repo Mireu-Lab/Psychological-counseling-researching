44b50d30d5f4ae4f42b5cb631658c7753848e4e1
Mireu Lim <97509351+Mireu-Lab@users.noreply.github.com>
2026-04-11 14:54:29 +0000

    _add _build.sh script for gemini embedding setup_

---
+++ sessions/05_gemini_embedding/_build.sh
@@ -0,0 +1,5 @@
+go mod init github.com/mireu-lab/psy_research/session05_gemini_embedding
+go get cloud.google.com/go/bigquery
+go get golang.org/x/oauth2
+go get golang.org/x/oauth2/google
+go get google.golang.org/api/iterator
