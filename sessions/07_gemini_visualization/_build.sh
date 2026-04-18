f654a71e899c76d1181614164879406b68f780b3
Mireu Lim <97509351+Mireu-Lab@users.noreply.github.com>
2026-04-18 14:12:43 +0000

    Remove Golang implementation for Gemini visualization, including build scripts, module files, main logic, and generated artifacts.

--- sessions/07_gemini_visualization/_build.sh
+++
@@ -1,6 +0,0 @@
-go mod init github.com/mireu-lab/psy_research/session07_gemini_visualization
-go get cloud.google.com/go/bigquery
-go get github.com/go-echarts/go-echarts/v2/charts
-go get github.com/go-echarts/go-echarts/v2/opts
-go get google.golang.org/api/option
-go get github.com/linkedin/goavro/v2
